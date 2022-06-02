bc7ff056a70db9a1079070e9ac222bae66c03c96
aeahmed-rc <a.ahmed9316@gmail.com>
2022-06-01 21:10:52 -0400

    practicing with state

--- src/App.js
+++ src/App.js
@@ -1,68 +1,17 @@
-const Header =(props) => {
+import { useState } from "react"
+
+
+const App = ()=>{
+  const [counter,setCounter] =useState(0)
+  setTimeout(
+    ()=> setCounter(counter+1),
+    1000
+  )// setCounter is called 1 second after setTimeout is called
+  console.log('rendering',counter)
   return (
-    <div>
-      <h1>
-      {props.course.name}
-      </h1>
-    </div>
-  )
-}
-const Part =(props)=>{
-  console.log('Part',props)
-  return(
-<div>
-<p>{props.part.parts.parts[0].exercises} {props.part.parts.parts[0].name}</p>
-<p>{props.part.parts.parts[1].exercises} {props.part.parts.parts[1].name}</p>
-<p>{props.part.parts.parts[2].exercises} {props.part.parts.parts[2].name}</p>
-</div>
-  )
-}
-const Content =(props)=>{
-  console.log('Content',props.parts.parts[0])
-  return(
-    <div>
-     <Part part= {props} />
-    </div>
-  )
-}
-const Total =(props) =>{
-  return(
-    <div>
-      <p> Total is {props.parts.parts[0].exercises+props.parts.parts[1].exercises+ props.parts.parts[2].exercises}</p>
-    </div>
-  )
-}
-
-
-const App = () => {
- 
-  const course ={
-    name:'Half Stack application development',
-   parts:[
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-}
-
-  return (
-    <div>
-      <Header course={course}/>
-      <Content parts={course}/>
-      <Total parts={course}/>
-      
-      
-    </div>
+    <div>{counter}</div>
   )
 }
 
 export default App
+
