879a7ea3daf7d776ca72904d53f26f08060c5d0d
aeahmed-rc <a.ahmed9316@gmail.com>
2022-06-06 16:13:33 -0400

    react conditionals

--- src/App.js
+++ src/App.js
@@ -5,35 +5,60 @@ const Display = ({counter})=><div>{counter}</div>
   
 
 
-const Button = ({onClick,text})=>  ( 
-<button onClick={onClick}>
-  {text}
-</button>
+
+
+ 
+ 
+const History =(props)=>{
+  if(props.allClicks.length ===0){
+    return(
+      <div>
+        click a button to see a result
+      </div>
+    )
+  }return(
+    <div>
+      this is the result, {props.allClicks.join(' ')}
+    </div>
   )
-
- 
- 
-
+}
+const Button =(props)=>{
+  return(
+    <div>
+      {props.value}
+  <button onClick={props.handleClick}>{props.text}</button> 
+    </div>
+     
+     )
+  
+}
 const App = ()=>{
-  const [counter,setCounter] =useState(0)
+  const [left,setLeft] = useState(0)
+  const [right,setRight] = useState(0)
+  const [allClicks,setAll] = useState([])
 
-  const increaseByOne = ()=> {
-    setCounter(counter+1)
-    console.log('clicked')
+  const handleLeftClick=()=>{
+    setAll(allClicks.concat('L'))
+    setLeft(left+1)
   }
-  const setToZero = ()=>{
-    setCounter(0)
+  const handleRightClick=()=>{
+    setAll(allClicks.concat('R'))
+    setRight(right+1)
   }
-  const decreaseByOne =()=>{
-    setCounter(counter-1)
+
+  const reset = ()=>{
+    setAll([])
+    setRight(0)
+    setLeft(0)
   }
-  console.log('rendering',counter)
+
   return (
     <div>
-    <Display counter={counter}/>
-    <Button onClick={increaseByOne} text='plus'/>
-    <Button onClick={setToZero} text='zero'/>
-    <Button onClick={decreaseByOne} text='minus'/>
+    <Button handleClick={handleLeftClick} text="left" value={left}/>
+    <Button handleClick={handleRightClick} text="right" value={right}/>
+    <Button handleClick={reset} text="reset"/>
+    <History allClicks={allClicks}/>
+    
     </div>
   )
 }
