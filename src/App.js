e1974d2ef63e0499c6150bedc309a4a84e4865dd
aeahmed-rc <a.ahmed9316@gmail.com>
2022-06-27 15:44:44 -0400

    adding data and retriving from server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import AddInfo from './Components/AddInfo'
+import axios from 'axios'
+
+
 
 const Info =(props)=>{
 return(
@@ -10,6 +13,8 @@ return(
 }
 
 const App = () => {
+
+
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' , phonenumber:'781',id:1},
     { name: 'Ada Lovelace', phonenumber: '39-44-5323523', id: 2 },
@@ -17,6 +22,14 @@ const App = () => {
     { name: 'sam', phonenumber: '12-43-234345', id: 4 },
 
   ]) 
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('response',response.data)
+      })
+  }, [])
   
   const [filtered,setFiltered] = useState('')
 
